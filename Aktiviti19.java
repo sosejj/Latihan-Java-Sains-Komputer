b1d6c0c6a629d7c566ddff054b758b4c92ead4bc
Shi Yang <shiyangsaw@gmail.com>
2022-05-28 20:39:08 +0800

    updated switch case

--- Aktiviti19.java
+++ Aktiviti19.java
@@ -7,38 +7,48 @@ public class Aktiviti19 {
         Scanner input = new Scanner (System.in);
         System.out.println ("Masukkan markah di antara 1 hingga 100: ");
         int markah = (int) input.nextDouble();
-        switch (markah) {
-            case 1:{}case 2:{}case 3:{}case 4:{}case 5:{}case 6:{}case 7:{}case 8:{}case 9:{}case 10:{}case 11:{}case 12:{}case 13:{}case 14:{}case 15:{}case 16:{}case 17:{}case 18:{}case 19:{}case 20:{}case 21:{}case 22:{}case 23:{}case 24:{}case 25:{}case 26:{}case 27:{}case 28:{}case 29:{}case 30:{}case 31:{}case 32:{}case 33:{}case 34:{}case 35:{}case 36:{}case 37:{}case 38:{}case 39:{}
-                output(markah,"G");
+        switch ((90 <= markah && markah <= 100) ? 1 :
+        (80 <= markah && markah <= 89) ? 2 :
+        (75 <= markah && markah <= 79) ? 3 :
+        (70 <= markah && markah <= 74) ? 4 :
+        (65 <= markah && markah <= 69) ? 5 :
+        (60 <= markah && markah <= 64) ? 6 :
+        (50 <= markah && markah <= 59) ? 7 :
+        (45 <= markah && markah <= 49) ? 8 :
+        (40 <= markah && markah <= 44) ? 9 :
+        (0 <= markah && markah <= 39) ? 10 : 11) 
+        {
+            case 1: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'A+'");
                 break;
-            case 40:{}case 41:{}case 42:{}case 43:{}case 44:{}
-                output(markah,"E");
+            } case 2: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'A'");
                 break;
-            case 45:{}case 46:{}case 47:{}case 48:{}case 49:{}
-                output(markah,"D");
+            } case 3: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'A-'");
                 break;
-            case 50:{}case 51:{}case 52:{}case 53:{}case 54:{}case 55:{}case 56:{}case 57:{}case 58:{}case 59:{}
-                output(markah,"C");
+            } case 4: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'B+'");
                 break;
-            case 60:{}case 61:{}case 62:{}case 63:{}case 64:{}
-                output(markah,"C+");
+            } case 5: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'B'");
                 break;
-            case 65:{}case 66:{}case 67:{}case 68:{}case 69:{}
-                output(markah,"B");
+            } case 6: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'C+'");
                 break;
-            case 70:{}case 71:{}case 72:{}case 73:{}case 74:{}
-                output(markah,"B+");
+            } case 7: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'C'");
                 break;
-            case 75:{}case 76:{}case 77:{}case 78:{}case 79:{}
-                output(markah,"A-");
+            } case 8: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'D'");
                 break;
-            case 80:{}case 81:{}case 82:{}case 83:{}case 84:{}case 85:{}case 86:{}case 87:{}case 88:{}case 89:{}
-                output(markah,"A");
+            } case 9: {
+                System.out.println("Markah anda ialah " + markah + ", gred 'E'");
                 break;
-            case 90:{}case 91:{}case 92:{}case 93:{}case 94:{}case 95:{}case 96:{}case 97:{}case 98:{}case 99:{}case 100:{}
-                output(markah,"A+");
+            } case 10: {
+                System.out.println ("Markah anda ialah " + markah + ", gred 'G'");
                 break;
-            default: {
+            } default: {
                 System.out.println("Nombor yang dimasukkan bukan di antara 0 hingga 100");
             }
         }
