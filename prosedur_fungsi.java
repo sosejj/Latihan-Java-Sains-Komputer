7b1e1d71bc4647045721850731ad8ed979c9d363
Shi Yang <shiyangsaw@gmail.com>
2023-01-07 16:15:52 +0800

    added prosedur_fungsi example

---
+++ prosedur_fungsi.java
@@ -0,0 +1,13 @@
+public class prosedur_fungsi {
+    // static int cariJumlah(int x, int y) {
+    //     return x + y;
+    // }
+    static void cariJumlah(int x, int y){
+        System.out.println(x+y);
+    }    
+    public static void main(String[] args) {
+        // int abc = cariJumlah(2, 4);
+        // System.out.println(abc);
+        cariJumlah(2, 6);
+    }   
+}
